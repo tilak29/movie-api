086f56243012bb9a3b4e0257310b27ecabc96f6f
tilakAdroja <tilakadroja29@gmail.com>
2023-04-21 01:09:23 +0530

    updated uaseEffect

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,43 +1,82 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import axios from 'axios'
+// import * as dotenv from 'dotenv' 
+
 
 const options = {
   method: 'GET',
   url: 'https://moviesdatabase.p.rapidapi.com/titles',
   headers: {
-    'X-RapidAPI-Key': ;
+    'X-RapidAPI-Key': import.meta.env.VITE_API_KEY,
+    'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+  }
+};
+
+const options1 = {
+  method: 'GET',
+  url: 'https://moviesdatabase.p.rapidapi.com/actors',
+  headers: {
+    'X-RapidAPI-Key': import.meta.env.VITE_API_KEY,
     'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
   }
 };
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [actors, setActors] = useState([]);
 
- useEffect(() => {
-  axios.request(options).then(function (response) {
-    console.log(response.data.results);
-    setMovies(response.data.results);
-  }).catch(function (error) {
-    console.error(error);
-  }); },[])
+//  useEffect(() => {
+//   axios.request(options).then(function (response) {
+//     console.log(response.data.results);
+//     // setMovies(response.data.results);
+//    
+//   }).catch(function (error) {
+//     console.error(error);
+//   }); },[])
+
+  // useEffect(() => {
+  //   axios.request(options1).then(function (response) {
+  //     console.log(response.data.results);
+  //     setActors(response.data.results);
+  //   }).catch(function (error) {
+  //     console.error(error);
+  //   }); },[])
+
+//Multiple Axios request in same useEffect:
+//https://stackoverflow.com/questions/61385454/how-to-post-multiple-axios-requests-at-the-same-time
+  useEffect(() => {
+  axios.all([
+    axios.request(options), 
+    axios.request(options1)
+  ])
+  .then(axios.spread((obj1, obj2) => {
+    // Both requests are now complete
+    console.log(obj1.data.results),
+    setMovies(obj1.data.results),
+    console.log(obj2.data.results),
+    setActors(obj2.data.results)
+  }));
+}, [])
+  
 
   return (
-    <div className="App">
+    <div style={{color:"white"}}>
       <p>shas</p>
 
       {/* actors api */}
-      {/* {
-        movies.map((val) => {
+      {
+        actors.map((val) => {
             return(
               <div key={val.nconst}>
-              <p>name: {val.primaryName} | 
+              
+              <p>Actor name: {val.primaryName} | 
               birthday: {val.birthYear}
               </p>
               </div>
             )
         })
-      } */}
+      }
 
 
       {/* title api */}
@@ -45,8 +84,8 @@ function App() {
         movies.map((val) => {
             return(
               <div key={val.id}>
-              <p>name: {val.titleText.text} | 
-              birthday: {val.releaseYear.year}
+              <p>Title name: {val.titleText.text} | 
+              Release date: {val.releaseYear.year}
               </p>
               </div>
             )
